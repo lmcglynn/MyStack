1a743b493c2e57f361d670584056058d0b726611
lmcglynn <liamo.mcglynn@comcast.net>
2024-05-27 13:49:39 -0700

    it works?

--- encampmentmap/src/components/MapWithPins.js
+++ encampmentmap/src/components/MapWithPins.js
@@ -4,14 +4,16 @@ import './MapWithPins.css';
 const MapWithPins = ({ pins, onPinClick }) => {
   return (
     <div className="map-container">
-      {pins.map((pin, index) => (
-        <div
-          key={index}
-          className="pin"
-          style={{ top: `${pin.top}%`, left: `${pin.left}%` }}
-          onClick={() => onPinClick(pin)}
-        />
-      ))}
+      <div className="map-content">
+        {pins.map((pin, index) => (
+          <div
+            key={index}
+            className="pin"
+            style={{ top: `${pin.top}%`, left: `${pin.left}%` }}
+            onClick={() => onPinClick(pin)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
