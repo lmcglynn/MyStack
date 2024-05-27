44bcb63320bbc43b2b49d7fe5fccee7f5d4e023b
lmcglynn <liamo.mcglynn@comcast.net>
2024-05-26 19:17:15 -0700

    add temp background

--- encampmentmap/src/App.js
+++ encampmentmap/src/App.js
@@ -1,16 +1,21 @@
 import Popup from './components/Popup';
+import { useState } from 'react';
 
 function App() {
+  const [buttonPopup, setButtonPopup] = useState(false);
+
   return (
     <div className="App">
       <main>
         <h1>Encampment Map</h1>
-        <br/><br/>
-        <button>Open Popup</button>
-        <Popup trigger={true}>
-          <h3>My popup</h3>
-        </Popup>
+        <br /><br />
+        <button onClick={() => setButtonPopup(true)}>Open Popup</button>
       </main>
+
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h3>My popup</h3>
+        <p>This is my popup</p>
+      </Popup>
     </div>
   );
 }
