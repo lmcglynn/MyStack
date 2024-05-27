f246d8479f234c3ab972be70c4572867b5864eae
lmcglynn <liamo.mcglynn@comcast.net>
2024-05-26 18:54:02 -0700

    add popup

--- encampmentmap/src/App.js
+++ encampmentmap/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import popup from './components/popup'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Encampment Map</h1>
+        <br/><br/>
+        <button>Open Popup</button>
+        <popup trigger={true}>
+          <h3>My popup</h3>
+        </popup>
+      </main>
     </div>
   );
 }
