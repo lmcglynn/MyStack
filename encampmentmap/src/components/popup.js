0a20c44fbf7567aabcf2e21febbd54fed0482654
lmcglynn <liamo.mcglynn@comcast.net>
2024-05-26 19:42:58 -0700

    added pin icon

--- encampmentmap/src/components/popup.js
+++ encampmentmap/src/components/popup.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Popup.css'
+import pinIcon from '../images/pin-icon.png'
 
 function popup(props) {
   return (props.trigger) ? (
     <div className="popup">
         <div className="popup-inner">
-            <button className="close-btn" onClick={() => props.setTrigger(false)}>Close</button>
+            <button className="close-btn" onClick={() => props.setTrigger(false)}> <img src={pinIcon}/> Close</button>
             { props.children }
         </div>
     </div>
