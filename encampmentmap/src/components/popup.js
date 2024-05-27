fb67f66d5ef05a3d51f8f26ad6922cea153f3a30
lmcglynn <liamo.mcglynn@comcast.net>
2024-05-26 19:51:14 -0700

    move images

--- encampmentmap/src/components/popup.js
+++ encampmentmap/src/components/popup.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Popup.css'
-import pinIcon from '../images/pin-icon.png'
+import pinIcon from '../public/images/pin-icon.png'
 
 function popup(props) {
   return (props.trigger) ? (
     <div className="popup">
         <div className="popup-inner">
-            <button className="close-btn" onClick={() => props.setTrigger(false)}> <img src={pinIcon}/> Close</button>
+            <button className="close-btn" onClick={() => props.setTrigger(false)}> <img src={pinIcon} alt={"button"} className="icon-image" /> Close</button>
             { props.children }
         </div>
     </div>
