c36710cd1ca51fa1fd88b0f4e9c3c2b0c88373a7
lmcglynn <liamo.mcglynn@comcast.net>
2024-06-02 18:05:36 -0700

    map working

--- encampmentmap/src/App.js
+++ encampmentmap/src/App.js
@@ -1,14 +1,94 @@
 import Popup from './components/Popup.js';
 import { useState } from 'react';
 import PinButton from './components/PinButton.js';
+import { MapContainer, TileLayer, useMap, Marker } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import pinIcon from './images/map-pin.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+const customIcon = L.icon({
+  iconUrl: pinIcon,
+  iconSize: [24, 24], // size of the icon
+  iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+  popupAnchor: [1, -34], // point from which the popup should open relative to the iconAnchor
+  // shadowUrl: 'leaflet/dist/images/marker-shadow.png',
+  // shadowSize: [41, 41], // size of the shadow
+  // shadowAnchor: [12, 41] // point of the shadow which will correspond to marker's location
+});
 
 function App() {
   const [buttonPopup, setButtonPopup] = useState(false);
   const [title, setTitle] = useState();
+  const center = [34.072000, -118.442184];
+  const d = 0.0001;
+
+  const [sliderValue, setSliderValue] = useState(1);
+  const handleSliderChange = (e) => {
+    setSliderValue(e.target.value);
+  };
 
   return (
     <div className="App">
       <main>
+        <MapContainer center={center} zoom={17} minZoom={15} style={{ height: "90vh", width: "100vw", justifyContent: "center", alignItems: "center" }}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {sliderValue == 1 && (
+            <Marker position={center} icon={customIcon} 
+              eventHandlers={{
+                click: () => {
+                  console.log('marker clicked')
+                  setButtonPopup(true)
+                  setTitle(0);
+                },
+              }}
+            />
+          )}
+          {sliderValue == 2 && (
+            <Marker position={[center[0]-2*d, center[1]]} icon={customIcon}
+              eventHandlers={{
+                click: () => {
+                  console.log('marker clicked')
+                  setButtonPopup(true)
+                  setTitle(1);
+                },
+              }}
+            />
+          )}
+          {sliderValue == 2 && (
+            <Marker position={[center[0]-2*d, center[1]+2*d]} icon={customIcon}
+              eventHandlers={{
+                click: () => {
+                  console.log('marker clicked')
+                  setButtonPopup(true)
+                  setTitle(2);
+                },
+              }}
+            />
+          )}
+
+        </MapContainer>
+        <input
+          type="range"
+          min="1"
+          max="3"
+          value={sliderValue}
+          onChange={handleSliderChange}
+          className="slider"
+        />
+      </main>
+      <Popup popupTitle={title} trigger={buttonPopup} setTrigger={setButtonPopup} />
+    </div>
+  );
+}
+
+export default App;
+
+{/* <main>
         <div className="map-container">
           <div className="map-content">
             <PinButton name="photo0" className="photo0" onClick={() => { setButtonPopup(true); setTitle(0) }} />
@@ -35,11 +115,4 @@ function App() {
             <PinButton name="photo21" className="photo21" onClick={() => { setButtonPopup(true); setTitle(21) }} />
           </div>
         </div>
-      </main>
-
-      <Popup popupTitle={title} trigger={buttonPopup} setTrigger={setButtonPopup} />
-    </div>
-  );
-}
-
-export default App;
+      </main> */}
